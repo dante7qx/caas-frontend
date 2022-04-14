f0e909563fa5000e21b530b5ab820eb677608eb0
dante7qx <sunchao.0129@163.comm>
2022-04-14 14:04:50 +0800

    测试上传组件

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -23,6 +23,10 @@
     <el-dialog title="预览" :visible.sync="dialogVisible" width="450px">
       <img :src="dialogImageUrl" alt="" style="width: 400px; height: 400px;" />
     </el-dialog>
+    <el-row style="margin-top: 10px;">
+      <el-button type="primary" @click="hideUploadBtn(true)">隐藏</el-button>
+      <el-button type="success" @click="hideUploadBtn(false)">显示</el-button>
+    </el-row>
 	</div>
 </template>
 <script>
@@ -44,10 +48,14 @@
         this.dialogImageUrl = file.url;
         this.dialogVisible = true;
       },
-      hideUploadBtn() {
+      hideUploadBtn(hide) {
         var upload_div = document.querySelectorAll('div.ruisun-uploader-cover > div[tabindex="0"]')
         upload_div.forEach(function(el) {
-          el.style.cssText = "display: none;";
+          if(hide) {
+            el.style.cssText = "display: none;";
+          } else {
+            el.style.cssText = "display: '';";
+          }
         })
       }
 		}
