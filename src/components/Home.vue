085ac5a6e4f689fc370bb4440bea92314b51da77
dante7qx <sunchao.0129@163.comm>
2022-05-18 15:36:00 +0800

    添加级联选择示例

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="home">
-      <h1>欢迎来到 CaaS 容器云</h1>
-      <div v-html="info"></div>
+		<h1>欢迎来到 CaaS 容器云</h1>
+		<div v-html="info"></div>
   </div>
-  
 </template>
 
 <script>
 import info from '../assets/info.txt'
 
 export default {
-    data() {
-        return {
-            info
-        }
-    }
+	data() {
+		return {
+				info
+		}
+	}
 }
 </script>
 <style scoped>
