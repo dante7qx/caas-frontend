2d56bf96b8129d16d6827d58e2e7292142180a73
dante7qx <sunchao.0129@163.comm>
2022-06-13 16:19:29 +0800

    添加IP检测

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,34 @@
 <template>
   <div id="home">
 		<h1>欢迎来到 CaaS 容器云</h1>
+		<div class="IPClass">{{ ipPosition.province + ' - ' + ipPosition.city + ' - ' + ipPosition.ip}}</div>
 		<div v-html="info"></div>
   </div>
 </template>
-
 <script>
 import info from '../assets/info.txt'
 
 export default {
 	data() {
 		return {
-				info
+				info,
+				ipPosition: {}
 		}
+	},
+	mounted() {
+		this.ipPosition = JSON.parse(localStorage.getItem('UserIPPosition'))
+	},
+	methods: {
+	  getUserIP(onNewIP) {}
 	}
 }
 </script>
 <style scoped>
 #home div {
     text-align: center;
+}
+.IPClass {
+	color: #27a8e1;
+	font-size: 10px;
 }
 </style>
