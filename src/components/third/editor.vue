73f67d4c74a2431311d9facbadb8411d717a0032
dante7qx <sunchao.0129@163.comm>
2022-09-01 17:57:43 +0800

    丰富示例

--- src/components/third/editor.vue
+++ src/components/third/editor.vue
@@ -99,7 +99,7 @@ export default {
         menubar: this.disabled ? false : this.menubar,
         toolbar: this.disabled ? false : this.toolbar,
         // fontsize_formats: '12px 14px 16px 18px 20px 22px 24px 26px 36px 48px 56px', // 工具栏自定义字体大小选项
-        font_formats: "宋体='宋体';黑体='黑体';仿宋='仿宋';楷体='楷体';隶书='隶书';微软雅黑='微软雅黑';Andale Mono=andale mono,times;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Trebuchet MS=trebuchet ms,geneva;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings",
+        font_formats: "宋体='宋体';黑体='黑体';仿宋='仿宋';楷体='楷体';隶书='隶书';微软雅黑='微软雅黑';Andale Mono=andale mono,times;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Trebuchet MS=trebuchet ms,geneva;Verdana=verdana,geneva",
         branding: false,
         resize: false,
         default_link_target: '_blank',
